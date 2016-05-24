6fac41499d9eefbb7523cf6231a252cac3921e23
Thierry Cailleau <Thierry.Cailleau@evault.com>
2016-05-24 13:52:36 +0200

    Commit from PowerShell Studio 5/24/2016 1:52 PM

---
+++ Test1.ps1
@@ -0,0 +1,14 @@
+﻿<#	
+	.NOTES
+	===========================================================================
+	 Created with: 	SAPIEN Technologies, Inc., PowerShell Studio 2016 v5.2.122
+	 Created on:   	5/24/2016 7:06 AM
+	 Created by:   	CailleauThierry
+	 Organization: 	Private
+	 Filename: Test1.ps1  	
+	===========================================================================
+	.DESCRIPTION
+		first test of PSStudio
+#>
+$a = Get-Service
+$a | Out-Host
